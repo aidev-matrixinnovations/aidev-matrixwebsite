b42ffb8e82eed2ca14ee80d1e8539de88e83d593
Binitjha2000 <binitjha2000@gmail.com>
2024-12-25 17:25:03 +0530

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
-import Pricing from './components/Pricing';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Categories from './components/Categories';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import Homepage from './components/HomePage';
 import Maximizepricing from './components/Maximizepricing';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Services from './components/Services';
-import Project from './components/Project';
-import ContactUs from './components/ContactUs';
-import StrategyViewer from './components/StrategyViewer';
 import NoHeaderFooterLayout from './components/NoHeaderFooterLayout';
-/* chnages made by BINIT KUMAR :TSTING PUSH MAIN */
+import Pricing from './components/Pricing';
+import Project from './components/Project';
+import Services from './components/Services';
+import StrategyViewer from './components/StrategyViewer';
+
 const App = () => {
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route element={<HeaderFooterLayout />}>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/maximizepricing" element={<Maximizepricing />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/project" element={<Project />} />
-            <Route path="/contact" element={<ContactUs />} />
-          </Route>
-          <Route element={<NoHeaderFooterLayout />}>
-            <Route path="/strategy/:strategy" element={<StrategyViewer />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route element={<HeaderFooterLayout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/maximizepricing" element={<Maximizepricing />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Route>
+        <Route element={<NoHeaderFooterLayout />}>
+          <Route path="/strategy/:strategy" element={<StrategyViewer />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
